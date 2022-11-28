523207eb05888b98232029ace52f66a0c4118064
ZeeFcd <ZeeFcd@protonmail.ch>
2022-11-28 15:36:09 +0100

    first commit

---
+++ fileread.py
@@ -0,0 +1,27 @@
+import tensorflow
+import keras
+import numpy as np
+from keras.utils import load_img, img_to_array
+import random
+import os
+
+
+def input_target_split(train_dir, labels):
+    dataset = []
+    count = 0
+    for label in labels:
+        folder = os.path.join(train_dir, label)
+        for image in os.listdir(folder):
+            img = load_img(os.path.join(folder, image), target_size=(150, 150))
+            img = img_to_array(img)
+            img = img / 255.0
+            dataset.append((img, count))
+        print(f'\rCompleted: {label}', end='')
+        count += 1
+    random.shuffle(dataset)
+    X, y = zip(*dataset)
+
+    return np.array(X), np.array(y)
+
+
+
