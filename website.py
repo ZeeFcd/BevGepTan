5016560498c7c5945964169afc71f01ac4ff2a1d
ZeeFcd <ZeeFcd@protonmail.ch>
2022-12-05 16:15:24 +0100

    layout change

--- website.py
+++ website.py
@@ -4,14 +4,17 @@ import streamlit as st
 from PIL import Image
 acc = Image.open("correctness.png")
 loss = Image.open("loss.png")
-report = pd.read_csv('report.csv')
+report = pd.read_csv('report.csv', index_col=0)
 
-st.image(acc, clamp=True)
-st.image(loss, clamp=True)
+col1, col2 = st.beta_columns((1, 1))
+
+with col1:
+    st.image(acc, clamp=True)
+
+
+with col2:
+    st.image(loss, clamp=True)
+
 st.table(data=report)
-
-
-
-
 
 
