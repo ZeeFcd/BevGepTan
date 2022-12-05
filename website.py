62ff0b235c563cecb01fdbcba3ea2a3a8d06d033
ZeeFcd <ZeeFcd@protonmail.ch>
2022-12-05 15:43:05 +0100

    asd

--- website.py
+++ website.py
@@ -4,9 +4,11 @@ import streamlit as st
 
 acc = load_img("correctness.png")
 acc = img_to_array(acc)
+acc = acc / 255.0
 
 loss = load_img("loss.png")
 loss = img_to_array(loss)
+loss = loss / 255.0
 
 st.image(acc)
 st.image(loss)
